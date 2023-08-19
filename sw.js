bfdf08d4aa492ba3b65915768938d6593b9fad39
Michael DiLucca <mikeman525@gmail.com>
2023-08-19 16:43:48 +0800

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-96245af3717d404e86d4.js"
+    "url": "webpack-runtime-7d735a1e2d539fcc8a67.js"
   },
   {
     "url": "framework-025a10830d88a05b2804.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "1d619a8ba46ee1dcb6883285b62adf2b"
+    "revision": "f312ac694d79038592097143054b967b"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-9b1997f2483a747a5203.js"
